d5006f16b0653255454c5a5794f8ce9bddf9b75a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-01-08 18:02:53 +0100

    Added Register and Login pages (empty for now)

---
+++ Pets-UI/Pets-UI.Mvc/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pets_UI.Mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
