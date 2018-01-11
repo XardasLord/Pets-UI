88338e90c50b9567bac4c8cf7a13ec381d2c2886
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2018-01-11 19:11:21 +0100

    AnimalsToCare in a table. Details of the selected position available.

--- Pets-UI/Pets-UI.Mvc/Controllers/AnimalController.cs
+++ Pets-UI/Pets-UI.Mvc/Controllers/AnimalController.cs
@@ -2,10 +2,8 @@
 using Pets_UI.Mvc.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Pets_UI.Mvc.Controllers
@@ -52,6 +50,23 @@ namespace Pets_UI.Mvc.Controllers
             return View(animals);
         }
 
+        [HttpGet]
+        public async Task<ActionResult> Details(Guid animalId)
+        {
+            AnimalToCare animal = null;
+
+            try
+            {
+                animal = await GetAnimalToCareDetails(animalId);
+            }
+            catch(Exception e)
+            {
+                ViewBag.Message = e.Message;
+            }
+
+            return View(animal);
+        }
+
         private async Task<List<AnimalToCare>> GetAnimalsToCareAsync(bool archive = false)
         {
             List<AnimalToCare> animals = null;
@@ -81,10 +96,40 @@ namespace Pets_UI.Mvc.Controllers
             }
             catch (Exception)
             {
-                throw new Exception("There is some problem with the external API login system. Try again later.");
+                throw new Exception("There is some problem with the external API. Try again later.");
             }
 
             return animals;
+        }
+
+        private async Task<AnimalToCare> GetAnimalToCareDetails(Guid animalId)
+        {
+            AnimalToCare animal = null;
+            var url = $"http://www.pets.pawelkowalewicz.pl/animals_to_care/{animalId}";
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string jsondata = await response.Content.ReadAsStringAsync();
+                        animal = JsonConvert.DeserializeObject<AnimalToCare>(jsondata.Substring(1, jsondata.Length - 2));
+                    }
+                    else
+                    {
+                        throw new Exception("There is some problem with retrieving the animal details from the external API. Try again later.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("There is some problem with the external API. Try again later.");
+            }
+
+            return animal;
         }
     }
 }
